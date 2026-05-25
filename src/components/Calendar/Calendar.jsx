0f9ae338022c63bdac82e7bc670cd7dc0aae2cce
lolvansh <lolvansh577@gmail.com>
2026-05-25 15:59:56 +0530

    docker files

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -12,7 +12,7 @@ const Calendar = () => {
 
   const getDaysInMonth = (year, month) => {
     return new Date(year, month + 1, 0).getDate();
-  };
+ };
 
   const getFirstDayOfMonth = (year, month) => {
     return new Date(year, month, 1).getDay();
